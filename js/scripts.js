d5d73392819495e691e7c6eba6e084ffd29f87df
ReneFarias <renefariasfeitosadasilva@gmail.com>
2023-05-31 01:27:20 -0300

    notification bug-1
    1 notificação só

--- js/scripts.js
+++ js/scripts.js
@@ -57,3 +57,17 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+/* notification */
+
+window.onload=function() {
+    var E = document.getElementsByClassName("item");
+    var m = E.length;
+    var n = parseInt(Math.random()*m);
+    for (var i=m-1;i>=0;i--) {
+        var e = E[i];
+        e.style.display='none';
+    }
+    E[n].style.display='block';
+  }
